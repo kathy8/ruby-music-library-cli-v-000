d7c1c15f88bae623fe8329cbcf2401c14f4b8650
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2018-01-31 07:22:27 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -21,43 +21,42 @@ class Song
   @@all << self
   end
 
- +  def artist=(artist)
- +    @artist=artist
- +    artist.add_song(self)
- +  end
- +
- +  def genre=(genre)
- +    @genre = genre
- +    if genre.songs.include?(self)
- +    else genre.songs << self
- +    end
- +  end
- +
- +  def self.create(name)
- +    song = self.new(name)
- +    song.save
- +    song
- +  end
- +
- +  def self.find_by_name(name)
- +    all.detect {|song| song.name == name}
- +  end
- +
- +  def self.find_or_create_by_name(name)
- +    find_by_name(name) || create(name)
- +  end
- +
- +  def self.new_from_filename(filename)
- +    artist = Artist.find_or_create_by_name(filename.split(' - ')[0])
- +    name = filename.split(' - ')[1]
- +    genre = Genre.find_or_create_by_name(filename.split(' - ')[2].chomp('.mp3'))
- +    Song.new(name, artist, genre)
- +  end
- +
- +  def self.create_from_filename(filename)
- +    self.new_from_filename(filename).tap(&:save)
- +  end
- +
- +end
-View
-11  lib/concerns/findable.rb
+ def artist=(artist)
+  @artist=artist
+  artist.add_song(self)
+  end
+
+  def genre=(genre)
+  @genre = genre
+  if genre.songs.include?(self)
+  else genre.songs << self
+  end
+  end
+
+  def self.create(name)
+    song = self.new(name)
+    song.save
+    song
+  end
+
+def self.find_by_name(name)
+all.detect {|song| song.name == name}
+end
+
+def self.find_or_create_by_name(name)
+find_by_name(name) || create(name)
+end
+
+def self.new_from_filename(filename)
+artist = Artist.find_or_create_by_name(filename.split(' - ')[0])
+name = filename.split(' - ')[1]
+genre = Genre.find_or_create_by_name(filename.split(' - ')[2].chomp('.mp3'))
+Song.new(name, artist, genre)
+end
+
+def self.create_from_filename(filename)
+self.new_from_filename(filename).tap(&:save)
+ end
+ 
+ end
+
